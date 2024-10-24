952498f5e49d43faaf5db0717fcff8aa201f411d
oopserved <saranghaeoppa412@gmail.com>
2024-10-24 02:02:08 -0500

    Update about.ts

--- src/data/about.ts
+++ src/data/about.ts
@@ -15,7 +15,7 @@ export const socialsDesc =
 export const teamsDesc =
   "Being part of a team in Code Coogs gives members the opportunity to hone their coding skills while making worthwhile friends. Members will work with a diverse group of fellow programmers, and people from all coding and cultural backgrounds. Members will get to experience a collective effort from their own team in producing a hefty end of year project showcased at our banquet";
 export const competitionsDesc =
-  "Every two weeks we will hold a competition that will put your skills to the test. Each competition will push you to become a better programmer. The skills you gain from solving these problems will show when you do technical interviews!";
+  "uhsbe"
 export const workshopsDesc =
   "USBE"
 export const officers = allOfficers;
